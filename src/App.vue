bd7699320daa89da868963a68ef0a21339dae19b
xiexiaotian <xiexiaotian9527@163.com>
2017-12-12 21:09:22 +0800

    初始化vue项目

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    1111111111111111111111111
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
